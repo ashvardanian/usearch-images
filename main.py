bb9b942f9fddba1ef95bf1d51615c4ba225fd4eb
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-06-04 18:01:55 +0400

    Refactor: Resource caching mechanism

--- main.py
+++ main.py
@@ -4,53 +4,83 @@ import streamlit as st
 import pandas as pd
 
 from uform import get_model
-from usearch.index import Index
+from usearch.index import Index, MetricKind
 
-# Page setup
+# Managing data
+
+data_path = os.environ.get('UNSPLASH_SEARCH_PATH')
+
+
+@st.cache_resource
+def get_unum_model():
+    return get_model('unum-cloud/uform-vl-english')
+
+
+@st.cache_resource
+def get_unsplash_metadata():
+    path = os.path.join(data_path, 'images.csv')
+    assert os.path.exists(path), 'Missing metadata file'
+    return pd.read_csv(path, dtype=str).fillna('')
+
+
+@st.cache_resource
+def get_unsplash_index():
+    index = Index(ndim=256, metric=MetricKind.Cos)
+    path = os.path.join(data_path, 'images.usearch')
+    assert os.path.exists(path), 'Missing index file'
+    index.view(path)
+    return index
+
+
+# GUI setup
+
 st.set_page_config(
     page_title='USearch through Unsplash',
-    page_icon='🐍', layout='wide')
+    page_icon='🐍', layout='wide',
+    initial_sidebar_state='collapsed',
+)
+
 st.title('USearch through Unsplash')
 
-data_path = '/Users/av/github/unsplash-search/'
-if 'table' not in st.session_state:
-    st.session_state['table'] = pd.read_csv(
-        os.path.join(data_path, 'images.csv'),
-        dtype=str).fillna('')
+slot_search_bar, _, slot_uform_ai, slot_captions = st.columns((16, 1, 2, 2))
 
-if 'model' not in st.session_state:
-    st.session_state['model'] = get_model('unum-cloud/uform-vl-english')
+with slot_search_bar:
+    query = st.text_input(
+        'Search Bar',
+        placeholder='Search for Unsplash photos',
+        value='', key='query', label_visibility='collapsed')
 
-if 'index' not in st.session_state:
-    index = Index(ndim=256, metric='cos')
-    index.view(os.path.join(data_path, 'images.usearch'))
-    st.session_state['index'] = index
+with slot_uform_ai:
+    use_ai = st.checkbox('UForm AI', value=True)
 
+with slot_captions:
+    show_captions = st.checkbox('Captions', value=True)
 
-query = st.text_input('Search for Unsplash photos', value='', key='query')
-columns = st.slider('Grid Columns', min_value=1, max_value=10, value=3)
-use_ai = st.checkbox('AI')
-show_captions = st.checkbox('Captions')
+columns = st.sidebar.slider('Grid Columns', min_value=1, max_value=10, value=3)
 
-
-table = st.session_state['table']
-model = st.session_state['model']
-index = st.session_state['index']
+table = get_unsplash_metadata()
+model = get_unum_model()
+index = get_unsplash_index()
 results = []
 max_caption_length = 100
 max_results = columns * 20
 
+# Search Content
+
 if not len(query):
     results = table[:max_results]
+
 elif use_ai:
     query_data = model.preprocess_text(query)
     query_embedding = model.encode_text(query_data).detach().numpy()
     matches, _, _ = index.search(query_embedding.flatten(), max_results)
     results = table.iloc[matches]
-    print('matches are', matches, results)
+
 else:
     results = table[table['photo_description'].str.contains(
         query)][:max_results]
+
+# Visualize Matches
 
 for n_row, row in results.reset_index().iterrows():
     i = n_row % columns
