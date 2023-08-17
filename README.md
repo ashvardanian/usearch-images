e581a39050e2fa57a7e612e34c9194626af918f1
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-08-17 16:46:48 +0400

    Docs: Shorter README

--- README.md
+++ README.md
@@ -1,51 +1,38 @@
-#  Semantic Gallery with StreamLit
+#  USearch Images
+
+![USearch Images animated text to image multi-modal AI search](assets/usearch-images-slow.gif)
+
+## Semantic Search Demo with [UForm][uform], [USearch][usearch], and [UCall][ucall]
+
+- [x] Can run the GUI and the search part on the same or different server
+- [x] Comes with pre-constructed indexes for large datasets
+- [x] Supports text-to-image and image-to-image search
+
+To start the StreamLit demo app locally, you need to download just a couple of files:
 
 ```sh
+mkdir -p data/unsplash25k
+wget -O data/unsplash25k/images.txt https://huggingface.co/datasets/unum-cloud/gallery-unsplash25k/resolve/main/images.txt
+wget -O data/unsplash25k/images.uform-vl-multilingual-v2.fbin https://huggingface.co/datasets/unum-cloud/gallery-unsplash25k/resolve/main/images.uform-vl-multilingual-v2.fbin
+
+pip install -r requirements.txt
 streamlit run streamlit_app.py
 ```
 
-To serve previews from the local dataset copy, please enable [static file serving](https://docs.streamlit.io/library/advanced-features/static-file-serving):
-
-```sh
-export STREAMLIT_SERVER_ENABLE_STATIC_SERVING=1
-```
+[uform]: https://github.com/unum-cloud/uform
+[usearch]: https://github.com/unum-cloud/usearch
+[ucall]: https://github.com/unum-cloud/ucall
 
 ## Datasets
 
-All datasets share identical format:
+The default dataset - Unsplash, contains less than 25'000 images.
+Still, the demo is easy to extend to other datasets, some of which we already embedded with UForm and indexed with USearch.
+All datasets are available on [Unum's HuggingFace page][unum-huggingface] and share an identical format:
 
 - `images.txt` contains newline-delimited URLs or Base64-encoded data-URIs of images.
 - `images.<model>.fbin` contains a binary matrix of [UForm][uform] embedding for every image from `images.txt`.
 - `images.<model>.usearch` contains a binary [USearch][usearch] search index for fast kANN.
 
-Additionally, some image-text paired datasets may provide `texts.txt`, `texts.fbin`, `texts.usearch`, following the same logic.
+Additionally, some image-text paired datasets may provide `texts.txt`, `texts.<model>.fbin`, `texts.<model>.usearch`, following the same logic.
 
-[uform]: https://github.com/unum-cloud/uform
-[usearch]: https://github.com/unum-cloud/usearch
-
-### Unsplash 25K
-
-```sh
-sh download.sh unsplash25k
-```
-
-Or just directly with `wget`:
-
-```sh
-mkdir -p datasets/unsplash25k
-wget -O datasets/unsplash25k/images.txt https://huggingface.co/datasets/unum-cloud/gallery-unsplash25k/resolve/main/images.txt
-wget -O datasets/unsplash25k/images.uform-vl-multilingual-v2.fbin https://huggingface.co/datasets/unum-cloud/gallery-unsplash25k/resolve/main/images.uform-vl-multilingual-v2.fbin
-wget -O datasets/unsplash25k/images.usearch https://huggingface.co/datasets/unum-cloud/gallery-unsplash25k/resolve/main/images.usearch
-```
-
-### CC 3M
-
-```sh
-sh download.sh cc3m
-```
-
-### LAION 400M
-
-```sh
-sh download.sh laion400m
-```
+[unum-huggingface]: https://huggingface.co/unum-cloud
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,37 +7,39 @@ import streamlit as st
 import PIL as pil
 from ucall.client import Client
 
+# Set Streamlit page configuration
 st.set_page_config(
     page_title="USearch Images",
-    page_icon="Ա",
+    page_icon="🎯",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
 
-# This following style patch would
-style = """
-<style>
-/* Container */
-[role="radiogroup"] {
-    display: flex;           /* Use flexbox for layout */
-    justify-content: space-between; /* Distribute items evenly across the container */
-    width: 100%;             /* Take up the full width of the parent */
-    flex-wrap: wrap;         /* Allow items to wrap to the next line if they don't fit */
-    gap: 10px;               /* Gap between items */
-}
-</style>
-"""
-st.markdown(style, unsafe_allow_html=True)
+# Style patch for radio buttons
+st.markdown(
+    """
+    <style>
+    [role="radiogroup"] {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        flex-wrap: wrap;
+        gap: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 st.title("USearch Images")
 
-ip_address: str = st.secrets["SERVER_IP"]
-ip_address = "0.0.0.0"
-ip_address = None
+# Server IP address
+ip_address = st.secrets.get("SERVER_IP", None)
 
 
 @st.cache_resource
 def get_server():
+    """Load server module locally, if no remote IP is set."""
     import server
 
     return server
@@ -45,85 +47,67 @@ def get_server():
 
 @st.cache_resource
 def get_examples_by_language() -> Dict[str, List[str]]:
+    """Load language examples from JSON."""
     with open("examples_by_language.json") as f:
         return json.load(f)
 
 
 @st.cache_resource
 def get_examples_vectors() -> Dict[str, np.ndarray]:
+    """Load example vectors from JSON."""
     with open("examples_vectors.json") as f:
         return json.load(f)
 
 
 def unwrap_response(resp):
-    if ip_address is None:
-        return resp
-    else:
-        return resp.json
+    """Extract response data based on the presence of IP address."""
+    return resp if ip_address is None else resp.json
 
 
-# Starting a new connection every time seems like a better option for now
-# @st.cache_resource
-# def get_client() -> Client:
-#     return Client(uri=ip_address)
-client = Client(uri=ip_address) if ip_address is not None else get_server()
-
-image_query = bytes()
-text_query = str()
-results = list()
-
-
-examples = get_examples_by_language()
+# Initialize primary variables
+client = Client(uri=ip_address) if ip_address else get_server()
+examples_by_language = get_examples_by_language()
 examples_vectors = get_examples_vectors()
 
-# This makes sure the search bar is populated on start,
-# even if no text was manually set and no example selected
-if "query" not in st.session_state:
-    st.session_state["query"] = examples["🇺🇸"][0]
-
-text_query: str = st.text_input(
+# Primary UI elements for search input
+st.session_state.setdefault("query", examples_by_language["🇺🇸"][0])
+text_query = st.text_input(
     "Search Bar",
     placeholder="USearch for Images in the Unsplash dataset",
     value=st.session_state["query"],
     key="text_query",
     label_visibility="collapsed",
 )
-
-image_query: io.BytesIO = st.file_uploader("Alternatively, choose an image file")
+image_query = st.file_uploader("Alternatively, choose an image file")
 selected_language = st.radio(
     "Or one of the examples",
-    list(examples.keys()),
+    list(examples_by_language.keys()),
     horizontal=True,
 )
 
 
 def handle_click(label):
+    """Update session state with selected example."""
     st.session_state.query = label
 
 
-example_widths = [len(x) for x in examples[selected_language]]
-example_columns = st.columns(example_widths)
+examples_widths = [len(x) for x in examples_by_language[selected_language]]
+examples_columns = st.columns(examples_widths)
 
-for i, example in enumerate(examples[selected_language]):
-    with example_columns[i]:
+for i, example in enumerate(examples_by_language[selected_language]):
+    with examples_columns[i]:
         if st.button(
             example, on_click=handle_click, args=(example,), use_container_width=True
         ):
             text_query = example
 
+# UI elements for search configuration are snucked into the side bar
 columns: int = st.sidebar.slider("Grid Columns", min_value=1, max_value=10, value=8)
-max_results: int = st.sidebar.number_input(
-    "Max Matches",
-    min_value=1,
-    max_value=None,
-    value=100,
-)
+max_results = st.sidebar.number_input("Max Matches", min_value=1, value=100)
 dataset_name: str = st.sidebar.selectbox("Dataset", ("unsplash25k",))
-size: int = unwrap_response(client.size(dataset_name))
+size = unwrap_response(client.size(dataset_name))
 
-
-# Search Content
-
+# Perform search, showing a spinning wheel in the meantime
 with st.spinner(f"We are searching through {size:,} entries"):
     if image_query:
         image_query = pil.Image.open(image_query).resize((224, 224))
@@ -134,6 +118,7 @@ with st.spinner(f"We are searching through {size:,} entries"):
                 count=max_results,
             )
         )
+    # Avoid AI inference if we can :)
     elif text_query in examples_vectors.keys():
         results = unwrap_response(
             client.find_vector(
@@ -151,20 +136,15 @@ with st.spinner(f"We are searching through {size:,} entries"):
             )
         )
 
-
+# Display Results
 st.success(
     f"Displaying {len(results):,} closest results from {size:,} entries!\nUses [UForm](https://github.com/unum-cloud/uform) AI model, [USearch](https://github.com/unum-cloud/usearch) vector search engine, and [UCall](https://github.com/unum-cloud/ucall) for remote procedure calls.",
     icon="✅",
 )
 
-
-# Visualize Matches
-
 for match_idx, match in enumerate(results):
     col_idx = match_idx % columns
     if col_idx == 0:
-        st.write("---")
-        cols = st.columns(columns, gap="large")
-
+        cols = st.columns(columns, gap="small")
     with cols[col_idx]:
         st.image(match, use_column_width="always")
