5b1bfd9dad79041b28c41471ddbe4bfbf2c89f80
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-09-02 15:44:54 +0400

    Refactor: Intro, analytics, download scripts

--- download.sh
+++ download.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 mkdir -p datasets/$1
-wget -c -O datasets/$1/images.fbin https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images.fbin
-wget -c -O datasets/$1/images.usearch https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images.usearch
+wget -c -O datasets/$1/images.fbin https://huggingface.co/datasets/unum-cloud/ann-$1/resolve/main/images.fbin
 
-if [ $1 = "cc3m" ]
+if [ $1 = "cc-3m" ]
 then
-    wget -c -O datasets/$1/images_part1.txt https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images_part1.txt
-    wget -c -O datasets/$1/images_part2.txt https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images_part2.txt
-    wget -c -O datasets/$1/images_part3.txt https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images_part3.txt
+    wget -c -O datasets/$1/images_part1.txt https://huggingface.co/datasets/unum-cloud/ann-$1/resolve/main/images_part1.txt
+    wget -c -O datasets/$1/images_part2.txt https://huggingface.co/datasets/unum-cloud/ann-$1/resolve/main/images_part2.txt
+    wget -c -O datasets/$1/images_part3.txt https://huggingface.co/datasets/unum-cloud/ann-$1/resolve/main/images_part3.txt
     cat datasets/$1/images_part1.txt datasets/$1/images_part2.txt datasets/$1/images_part3.txt > datasets/$1/images.txt && rm datasets/$1/images_part*.txt
-    wget -c -O datasets/$1/texts.fbin https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/texts.fbin
-    wget -c -O datasets/$1/texts.usearch https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/texts.usearch
 else
-    wget -c -O datasets/$1/images.txt https://huggingface.co/datasets/unum-cloud/gallery-$1/resolve/main/images.txt
+    wget -c -O datasets/$1/images.txt https://huggingface.co/datasets/unum-cloud/ann-$1/resolve/main/images.txt
 fi
--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,40 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# Set up Google Analytics
+google_analytics = (
+    st.secrets.get("GOOGLE_ANALYTICS", None)
+    if os.path.exists(".streamlit/secrets.toml")
+    else None
+)
+if not google_analytics:
+    google_analytics = "G-71Y7G3QKXQ"
+
+st.markdown(
+    """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={ga}"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '{ga}');
+    </script>
+    """.format(
+        ga=google_analytics
+    ),
+    unsafe_allow_html=True,
+)
+
 st.title("USearch Images")
 
 # Server IP address
-ip_address = st.secrets.get("SERVER_IP", None) if os.path.exists(".streamlit/secrets.toml") else None
+ip_address = (
+    st.secrets.get("SERVER_IP", None)
+    if os.path.exists(".streamlit/secrets.toml")
+    else None
+)
 
 
 @st.cache_resource
@@ -139,11 +169,28 @@ with st.spinner(f"We are searching through {size:,} entries"):
 
 # Display Results
 st.success(
-    f"Displaying {len(results):,} closest results from {size:,} entries!\nUses [UForm](https://github.com/unum-cloud/uform) AI model, [USearch](https://github.com/unum-cloud/usearch) vector search engine, and [UCall](https://github.com/unum-cloud/ucall) for remote procedure calls.",
+    f"Displaying {len(results):,} closest results from {size:,} entries.",
     icon="✅",
 )
 
-for match_idx, match in enumerate(results):
+st.markdown(
+    """
+    This page serves as a proof-of-concept for constructing a multi-lingual, multi-modal Semantic Search system with just 50 lines of Python code. 
+    The content is encoded using the {uform} multi-modal transformer model, which produces embeddings in a shared vector space for text queries and visual content.
+    These embeddings are then indexed and queried via the {usearch} vector search engine.
+    For client-server interaction, the stack utilizes {ucall} for remote procedure calls.
+    Each component in this architecture is optimized for high throughput, making it scalable for large datasets.
+    After the initial retrieval, results can be further refined using larger neural network models before being presented to the end-user.
+    """.format(
+        uform="[![UForm stars](https://img.shields.io/github/stars/unum-cloud/uform?style=social&label=UForm)](https://github.com/unum-cloud/uform)",
+        usearch="[![USearch stars](https://img.shields.io/github/stars/unum-cloud/usearch?style=social&label=USearch)](https://github.com/unum-cloud/usearch)",
+        ucall="[![UCall stars](https://img.shields.io/github/stars/unum-cloud/ucall?style=social&label=UCall)](https://github.com/unum-cloud/ucall)",
+    )
+)
+
+max_rows = 4
+
+for match_idx, match in enumerate(results[: columns * max_rows]):
     col_idx = match_idx % columns
     if col_idx == 0:
         cols = st.columns(columns, gap="small")
